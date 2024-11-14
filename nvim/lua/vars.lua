c5596fba2905aae2948b6a2f4661bd34f415492a
Peijmon Kasravi <kasravi.peijmon@gmail.com>
2024-11-13 21:20:59 -0800

    Added some more options from init.vim to lua

--- nvim/lua/vars.lua
+++ nvim/lua/vars.lua
@@ -1,5 +1,7 @@
 --[[ vars.lua ]]
 
-local g = vim.g
-g.t_co = 256
-g.background = "dark"
+local g = vim.g            -- Set globally
+g.t_co = 256               -- int: Terminal colors
+g.background = "dark"      -- str: Color scheme (light/dark)
+g.netrw_liststyle = 3      -- num: Change directory tree style
+g.netrw_banner = false     -- bool: Enable/disable netrw banner
